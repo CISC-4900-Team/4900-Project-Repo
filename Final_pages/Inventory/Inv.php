6b767d94d506bcb7cad4cec3cc649d9ebb7ff51c
Andy Henry <A.henry250@gmail.com>
2020-04-11 17:39:28 -0400

    Last Changes

--- Final_pages/Inventory/Inv.php
+++ Final_pages/Inventory/Inv.php
@@ -23,6 +23,14 @@
       
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
+           <!-- DropDown4 Start -->
+           <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  System Usage
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Important Information</a>
+              </li>
                   <!-- DropDown4 Start -->
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
